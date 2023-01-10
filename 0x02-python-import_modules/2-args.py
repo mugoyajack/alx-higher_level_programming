8a4477f7be68807aa12f4ce6840ff269199b021d
Jack Mugoya <mugoyajack3@gmail.com>
2023-01-10 16:35:59 +0300

    0x02. Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-c = len(sys.argv) - 1
-if c == 0:
-    print("0 arguments.")
-elif c == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(c))
-for i in range(c):
-    print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    c = len(sys.argv) - 1
+    if c == 0:
+        print("0 arguments.")
+    elif c == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(c))
+    for i in range(c):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-c = len(sys.argv) - 1
-sum = 0
-for i in range(c):
-    sum = sum + int(sys.argv[1 + i])
-print("{0}".format(sum))
+if __name__ == "__main__":
+    import sys
+    c = len(sys.argv) - 1
+    sum = 0
+    for i in range(c):
+        sum = sum + int(sys.argv[1 + i])
+    print("{0}".format(sum))
