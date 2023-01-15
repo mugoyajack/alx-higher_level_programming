a5579ddd69c6bfcc21f75316117ef9bfe8fe7083
Jack Mugoya <mugoyajack3@gmail.com>
2023-01-15 18:22:04 +0300

    4 and 5

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""class square."""
+"""Class Square"""
 
 
 class Square:
+    """Class Square"""
     def __init__(self, size=0):
         """New square initialization
-        
+
         Args:
             size (int): Size of a square
         """
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
         """Return area of a square"""
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""class square."""
+"""Class Square"""
 
 
 class Square:
+    """Class Square"""
     def __init__(self, size=0):
         """New square initialization
-        
+
         Args:
             size (int): Size of a square
         """
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
         """Return area of a square"""
@@ -22,7 +27,6 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """Size of the square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
