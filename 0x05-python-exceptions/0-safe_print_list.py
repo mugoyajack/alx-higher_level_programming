ed04add8ec157bbca0a636fc0bfd6da492f535c5
Jack Mugoya <mugoyajack3@gmail.com>
2023-01-15 12:43:27 +0300

    0x05. Python - Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     for n in range(x):
         try:
-            print("{0}".format(my_list[n]), end="")
+            print("{:d}".format(my_list[n]), end="")
         except (IndexError):
             n = n - 1
             break
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             x = 0
         except (TypeError):
-            print("wrong_type")
+            print("wrong type")
             x = 0
         except (ZeroDivisionError):
             print("division by 0")
