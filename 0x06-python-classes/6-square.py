ab6ef1468935224d28504dfcd7308792a19c8ba4
Jack Mugoya <mugoyajack3@gmail.com>
2023-01-15 19:18:15 +0300

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Class Square"""
 
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         """New square initialization
 
         Args:
