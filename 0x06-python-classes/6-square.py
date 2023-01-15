eb7ded3ea53a8d0fe449c3120f875faa4ea2f4bf
Jack Mugoya <mugoyajack3@gmail.com>
2023-01-15 19:16:02 +0300

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Class Square"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0,0)):
         """New square initialization
 
         Args:
@@ -16,6 +16,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        self.__position = position
 
     def area(self):
         """Return area of a square"""
@@ -57,13 +58,14 @@ class Square:
         self.__position = value
 
     def my_print(self):
-        """Print square with #"""
+        """Print the square with the # character."""
         if self.__size == 0:
+
             print("")
             return
 
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("_", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
