c5f48b0ff0d5865e1a1639ace667e13b16f9d822
Jack Mugoya <mugoyajack3@gmail.com>
2023-01-15 12:53:44 +0300

    0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    c = 0
     for n in range(x):
         try:
             print("{:d}".format(my_list[n]), end="")
+            c += 1
         except (IndexError):
-            n = n - 1
             break
     print()
-    return n + 1
+    return c
