65f55c2560bdb433335eec0b52aa34f38c421a75
Jack Mugoya <mugoyajack3@gmail.com>
2023-01-16 20:17:05 +0300

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,10 +13,10 @@ class Square:
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """Return area of a square"""
@@ -32,7 +32,7 @@ class Square:
         """Size of the square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
